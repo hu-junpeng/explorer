31228f68c74cc780f76645cc80d5f477398efe62
e <362260879@qq.com>
2020-03-19 20:06:21 +0800

    第一次提交

---
+++ 浏览器/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace 浏览器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbBack = new System.Windows.Forms.ToolStripButton();
+            this.tsbForward = new System.Windows.Forms.ToolStripButton();
+            this.tsbRefresh = new System.Windows.Forms.ToolStripButton();
+            this.tsbHome = new System.Windows.Forms.ToolStripButton();
+            this.tsbStop = new System.Windows.Forms.ToolStripButton();
+            this.tsbViewSource = new System.Windows.Forms.ToolStripButton();
+            this.tsbExit = new System.Windows.Forms.ToolStripButton();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1370, 749);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "网址：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(635, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Open
+            // 
+            this.Open.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Open.Location = new System.Drawing.Point(700, 36);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 3;
+            this.Open.Text = "访问";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbBack,
+            this.tsbForward,
+            this.tsbRefresh,
+            this.tsbHome,
+            this.tsbStop,
+            this.tsbViewSource,
+            this.tsbExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1370, 31);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // tsbBack
+            // 
+            this.tsbBack.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbBack.Image = ((System.Drawing.Image)(resources.GetObject("tsbBack.Image")));
+            this.tsbBack.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbBack.Name = "tsbBack";
+            this.tsbBack.Size = new System.Drawing.Size(28, 28);
+            this.tsbBack.Text = "tsbBack";
+            // 
+            // tsbForward
+            // 
+            this.tsbForward.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbForward.Image = ((System.Drawing.Image)(resources.GetObject("tsbForward.Image")));
+            this.tsbForward.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbForward.Name = "tsbForward";
+            this.tsbForward.Size = new System.Drawing.Size(28, 28);
+            this.tsbForward.Text = "tsbForward";
+            // 
+            // tsbRefresh
+            // 
+            this.tsbRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbRefresh.Image = ((System.Drawing.Image)(resources.GetObject("tsbRefresh.Image")));
+            this.tsbRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbRefresh.Name = "tsbRefresh";
+            this.tsbRefresh.Size = new System.Drawing.Size(28, 28);
+            this.tsbRefresh.Text = "tsbRefresh";
+            // 
+            // tsbHome
+            // 
+            this.tsbHome.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbHome.Image = ((System.Drawing.Image)(resources.GetObject("tsbHome.Image")));
+            this.tsbHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbHome.Name = "tsbHome";
+            this.tsbHome.Size = new System.Drawing.Size(28, 28);
+            this.tsbHome.Text = "tsbHome";
+            // 
+            // tsbStop
+            // 
+            this.tsbStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbStop.Image = ((System.Drawing.Image)(resources.GetObject("tsbStop.Image")));
+            this.tsbStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbStop.Name = "tsbStop";
+            this.tsbStop.Size = new System.Drawing.Size(28, 28);
+            this.tsbStop.Text = "tsbStop";
+            // 
+            // tsbViewSource
+            // 
+            this.tsbViewSource.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbViewSource.Image = ((System.Drawing.Image)(resources.GetObject("tsbViewSource.Image")));
+            this.tsbViewSource.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbViewSource.Name = "tsbViewSource";
+            this.tsbViewSource.Size = new System.Drawing.Size(28, 28);
+            this.tsbViewSource.Text = "tsbViewSource";
+            // 
+            // tsbExit
+            // 
+            this.tsbExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbExit.Image = ((System.Drawing.Image)(resources.GetObject("tsbExit.Image")));
+            this.tsbExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbExit.Name = "tsbExit";
+            this.tsbExit.Size = new System.Drawing.Size(28, 28);
+            this.tsbExit.Text = "tsbExit";
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Location = new System.Drawing.Point(0, 65);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(1358, 684);
+            this.webBrowser2.TabIndex = 5;
+            this.webBrowser2.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser2_Navigated);
+            this.webBrowser2.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser2_NewWindow);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.webBrowser2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Hyman的浏览器";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbBack;
+        private System.Windows.Forms.ToolStripButton tsbForward;
+        private System.Windows.Forms.ToolStripButton tsbRefresh;
+        private System.Windows.Forms.ToolStripButton tsbHome;
+        private System.Windows.Forms.ToolStripButton tsbStop;
+        private System.Windows.Forms.ToolStripButton tsbViewSource;
+        private System.Windows.Forms.ToolStripButton tsbExit;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+    }
+}
+
---
+++ 浏览器/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace 浏览器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("User32.DLL")]
+        public static extern int SendMessage(IntPtr hWnd,uint Msg,int wParam,string Iparam);
+        [DllImport("User32.DLL")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent,IntPtr hwndChildAfter,string lpszWindow,string str);
+        public const uint WM_SETTEXT = 0x00C;
+        private void Open_Click(object sender, EventArgs e)
+        {
+            openPage();
+        }
+        private void openPage()
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                webBrowser2.Navigate(textBox1.Text.Trim());
+            }
+            else
+            {
+                MessageBox.Show("请正确输入你要访问的网址！！包括http://");
+            }
+        }
+
+        private void webBrowser2_NewWindow(object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            if (webBrowser2.Document.ActiveElement != null)
+            {
+                webBrowser2.Navigate(webBrowser2.Document.ActiveElement.GetAttribute("href"));
+                textBox1.Text =webBrowser2.Document.ActiveElement.GetAttribute("href");
+            }
+        }
+
+        private void webBrowser2_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            if (webBrowser2.CanGoBack)
+            {
+                toolStrip1.Items[0].Enabled = true;
+            }
+            else
+                toolStrip1.Items[0].Enabled = false;
+            if(webBrowser2.CanGoForward)
+            {
+                toolStrip1.Items[1].Enabled = true;
+            }
+            else
+                toolStrip1.Items[1].Enabled = false;
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            try
+            { 
+                if(e.ClickedItem.Name=="tsbBack")
+                {
+                    webBrowser2.GoBack();
+                }
+                if (e.ClickedItem.Name=="tsbForward")
+                {
+                    webBrowser2.GoForward();
+                }
+                if (e.ClickedItem.Name == "tsbRefresh")
+                {
+                    webBrowser2.Refresh();
+                }
+                if(e.ClickedItem.Name=="tsbHome")
+                {
+                    webBrowser2.GoHome();
+                }
+                if(e.ClickedItem.Name=="tsbStop")
+                {
+                    webBrowser2.Stop();
+                }
+                if(e.ClickedItem.Name=="tsbExit")
+                {
+                    if (MessageBox.Show("确认退出浏览器吗？", "退出对话框", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    {
+                        Application.Exit();
+                    }
+                }
+                if (e.ClickedItem.Name == "tsbViewSource")
+                {
+                    WebRequest wrq = WebRequest.Create(textBox1.Text);
+                    WebResponse wrs = wrq.GetResponse();
+                    StreamReader sr = new StreamReader(wrs.GetResponseStream(),Encoding.Default);
+                    string code = "";
+                    while(sr.ReadLine()!=null)
+                    {
+                        code += sr.ReadLine();
+                    }
+                    System.Diagnostics.Process pro = new System.Diagnostics.Process();
+                    pro.StartInfo.FileName = "notepad.exe";
+                    pro.StartInfo.RedirectStandardInput = true;
+                    pro.StartInfo.RedirectStandardOutput = true;
+                    pro.StartInfo.UseShellExecute = false;
+                    pro.Start();
+                    if(pro!=null)
+                    {
+                        while(pro.MainWindowHandle==IntPtr.Zero)
+                        {
+                            pro.Refresh();
+                        }
+                        IntPtr vHandle = FindWindowEx(pro.MainWindowHandle,IntPtr.Zero,"Edit",null);
+                        SendMessage(vHandle,WM_SETTEXT,0,code);
+                    }
+                }
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message.ToString()); }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char c = e.KeyChar;
+            if (c == 13) openPage();
+        }
+    }
+}
